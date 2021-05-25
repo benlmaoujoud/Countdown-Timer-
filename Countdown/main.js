db1e89e096135d9fabf1e74209ae4302bd1f5af7
Mohamed Benlmaoujoud <40149539+benlmaoujoud@users.noreply.github.com>
2021-05-25 15:49:47 +0100

    Update main.js

--- Countdown/main.js
+++ Countdown/main.js
@@ -22,7 +22,7 @@
                 countdown = document.getElementById("countdown"),
                 content = document.getElementById("content");
   
-            headline.innerText = "It's my timING!";
+            headline.innerText = "Baraka 3lik hh";
             countdown.style.display = "none";
             content.style.display = "block";
   
